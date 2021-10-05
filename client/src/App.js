f98dac0ae2ab3a2cc3f55853d36d2aadf427de15
Elias <asd>
2021-10-04 22:57:23 -0300

    hice un contador

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,18 @@
-const App = () => <h1>App</h1>;
+import { useState } from 'react';
+
+const App = () => {
+  const [numero, setNumero] = useState(0);
+const dec = () => { setNumero ( prev => prev-1 ) };
+const inc = () => { setNumero ( prev => prev+1 ) };
+const rest = () => { setNumero (0) };
+  return (
+    <div>
+      <h1> {numero} </h1>
+      <button onClick={inc} > + </button>
+      <button onClick={dec} > - </button>
+      <button onClick={rest} > rest </button>
+    </div>
+  );
+};
 
 export default App;
